649288dd11186f0e76f352aeaf59224ec831f5ce
shiyingying01 <718275303@qq.com>
2019-09-05 17:13:44 +0800

    包裹入库

---
+++ src/components/parcelStorage.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="parcelStorage">
+    <div>
+      <h1>包裹入库</h1>
+    </div>
+    <div>
+      <a-form :form="form" @submit="handleSubmit">
+        <a-form-item label="运单号" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
+          <a-input
+            v-decorator="[
+          'id',
+          {rules: [{ required: true, message: '请输入运单号!' }]}
+        ]"
+          />
+        </a-form-item>
+        <a-form-item label="收件人" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
+          <a-input
+            v-decorator="[
+          'receiver',
+          {rules: [{ required: true, message: '请输入收件人!' }]}
+        ]"
+          />
+        </a-form-item>
+        <a-form-item label="电话" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
+          <a-input
+            v-decorator="[
+          'phone',
+          {rules: [{ required: true, message: '请输入电话!' }]}
+        ]"
+          />
+        </a-form-item>
+        <a-form-item label="重量" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
+          <a-input
+            v-decorator="[
+          'weight',
+          {rules: [{ required: true, message: '请输入重量!' }]}
+        ]"
+          />
+        </a-form-item>
+        <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
+          <a-button type="primary" html-type="submit">提交</a-button>
+        </a-form-item>
+      </a-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "parcelStorage",
+  data() {
+    return {
+      formLayout: "horizontal",
+      form: this.$form.createForm(this)
+    };
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log("Received values of form: ", values);
+        }
+      });
+    }
+  }
+};
+</script>
+
--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'ant-design-vue/dist/antd.css'
+import antDesign from 'ant-design-vue'
 
 Vue.config.productionTip = false
+Vue.use(antDesign);
 
 new Vue({
   router,
